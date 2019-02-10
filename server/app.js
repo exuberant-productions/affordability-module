80c28aaca08205583e73cd0077e55137f4e390d8
Sang <Sang@Sang.webpass.net>
2019-02-09 17:19:38 -0800

    Fix eslint error

--- server/app.js
+++ server/app.js
@@ -1,33 +1,33 @@
-var express = require('express');
+const express = require('express');
 
-var db = require('../db/index.js');
+const db = require('../db/index.js');
 
-var app = express();
+const app = express();
 
 app.use(express.json());
 
 app.set('port', 3001);
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(`${__dirname}/../public`));
 
 app.get('/homeDetails/:homeId', (req, res) => {
-	db.getHome(req.params.homeId, (err, data) => {
-		if (err) {
-			console.error('error getting data from database');
-		} else {
-			res.send(data);
-		}
-	});
+  db.getHome(req.params.homeId, (err, data) => {
+    if (err) {
+      throw new Error('error getting data from database');
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 app.get('/similarHomes/:homeId', (req, res) => {
-	// do db stuff
-	res.send('success /similarHomes/' + req.params.homeId);
+  // do db stuff
+  res.send(`success /similarHomes/${req.params.homeId}`);
 });
 
 app.get('/affordability/:homeId', (req, res) => {
-	// do db stuff
-	res.send('success /affordability/' + req.params.homeId);
+  // do db stuff
+  res.send(`success /affordability/${req.params.homeId}`);
 });
 
 module.exports = app;
