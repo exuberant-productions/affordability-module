86698a77a09ea39728fbaa327f0880d2eb24de54
Sang <Sang@Sang.webpass.net>
2019-02-09 17:10:48 -0800

    Fix eslint errors

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
 
-	render() {
-		return (
-			<div>
-				Hello World, react/webpack is working correctly
-			</div>
-		);
-	}
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World, react/webpack is working correctly
+      </div>
+    );
+  }
 }
-
-
 
 export default App;
