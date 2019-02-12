46f5c0d0d31333ac3b193684c48507233db67cb0
Sang <Sang@Sang.webpass.net>
2019-02-11 17:24:09 -0800

    Implement sql query to get real data

--- db/index.js
+++ db/index.js
@@ -5,16 +5,37 @@ const mysqlConfig = require('./config.js');
 const connection = mysql.createConnection(mysqlConfig);
 
 const getHome = (homeId, callback) => {
-	// send query, process data, callback with data
-	connection.query('SELECT * FROM home WHERE id = ' + homeId, (err, data) => {
-		if (err) {
-			callback(err);
-		} else {
-			callback(null, data);
-		}
-	});
-}
+  // send query, process data, callback with data
+  let query = 'SELECT home.totalPrice as totalPrice, hd.description as description, ';
+  query += ' ov.type as ovType, ov.rooms as ovRooms, ov.baths as ovBaths, ov.builtIn as ovBuiltIn, ov.howlongOnTrulia as ovHowlongOnTrulia, ';
+  query += ' ov.lotSize as ovLotSize, ov.sqft as ovSqft, ov.pricePerSqft as ovPricePerSqft, ov.priceHOA as ovPriceHOA, ov.views as ovViews, ';
+  query += ' li.updatedAt as liUpdatedAt, li.rooms as liRooms, li.baths as liBaths, li.type as liType, li.sqft as liSqft, li.lotSize as liLotSize, ';
+  query += ' pr.updatedAt as prUpdatedAt, pr.rooms as prRooms, pr.baths as prBaths, pr.type as prType, pr.sqft as prSqft, pr.lotSize as prLotSize ';
+  query += ' FROM home, homeDetails AS hd, overview as ov, features as ft, listingInfo as li, publicRecords as pr ';
+  query += ' WHERE hd.id = home.detailsId AND ov.id = hd.overviewId AND ft.id = hd.featuresId AND li.id = ft.listingInfoId AND pr.id = ft.publicRecordsId';
+  query += ` AND home.id = ${homeId}`;
+  connection.query(query, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      query = 'SELECT ph.historyDate, ph.price, ph.event, ';
+      query += ' phd.recordingDate, phd.contractDate, phd.salePrice, phd.priceType, phd.countyTransferTax, phd.totalTransferTax, ';
+      query += ' phd.transactionType, phd.documentType, phd.priceChange, phd.source ';
+      query += ' FROM home, homeDetails as hd, priceHistory as ph, priceHistoryDetails as phd ';
+      query += ' WHERE hd.id = home.detailsId AND hd.id = ph.homeDetailsId AND phd.id = ph.detailsId ';
+      query += ` AND home.id = ${homeId}`;
+      connection.query(query, (err2, priceHistoryData) => {
+        if (err2) {
+          callback(err2);
+        } else {
+          data[0].priceHistoryData = priceHistoryData; // eslint-disable-line no-param-reassign
+          callback(null, data[0]);
+        }
+      });
+    }
+  });
+};
 
 module.exports = {
-	getHome
-}
+  getHome,
+};
