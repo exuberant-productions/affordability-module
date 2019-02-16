3a378c4622621930e2478d8a8b0eaacb7d2009d3
Sang <Sang@Sang.webpass.net>
2019-02-16 12:46:51 -0800

    Fix server test not receiving correct data

--- server/spec/server.spec.js
+++ server/spec/server.spec.js
@@ -1,11 +1,15 @@
-import "babel-polyfill";
-const request = require("supertest");
+import 'babel-polyfill';
+
+const request = require('supertest');
 const app = require('../app');
 
-describe("GET /homeDetails/1", () => {
-  test("It should respond with correct home details data", async () => {
-    const response = await request(app).get("/homeDetails/1");
-    expect(response.body).toEqual([{"id": 1, "totalPrice": 779213, "detailsId": 1}]);
+describe('GET /homeDetails/1', () => {
+  test('It should respond with correct home details data', async () => {
+    const response = await request(app).get('/homeDetails/1');
+    expect(response.body).toBeDefined();
+    expect(response.body.id).toEqual(1);
+    expect(response.body.totalPrice).toEqual(1216472);
+    expect(response.body.priceHistoryData).toBeInstanceOf(Array);
     expect(response.statusCode).toBe(200);
   });
 });
