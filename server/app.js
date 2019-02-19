cdef1b57fcbdecb823e873041f0f6b06cbec58bc
Sang <Sang@Sang.local>
2019-02-19 14:59:22 -0800

    Add cors header for express

--- server/app.js
+++ server/app.js
@@ -10,6 +10,12 @@ app.set('port', 3001);
 
 app.use(express.static(`${__dirname}/../public`));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/:homeId', (req, res) => {
   res.redirect(`/index.html?homeId=${req.params.homeId}`);
 });
